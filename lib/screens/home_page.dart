1ea6cc0e05d039d16558885c4f964de240a26b1c
RubenGomez1990 <rubengomez@paucasesnovescifp.cat>
2025-12-16 16:12:01 +0100

    Implementación home_page

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,1 +1,29 @@
+import 'package:flutter/material.dart';
 
+// Creamos HomePage como StatefulWidget porque necesitará cambiar su estado cuando
+// creemos otro de los widgets, concretamete PersonalPage
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  // Utilizamos createState para almacenar los datos y que flutter no redibuje todo el widget
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('PF2')),
+      body: Center(
+        child: Column(
+          children: [
+            Text("¡Bienvenidos!"),
+            ElevatedButton(onPressed: () {}, child: Text('Ir a Personal Page')),
+            ElevatedButton(onPressed: () {}, child: Text('Ir a Widget Page')),
+          ],
+        ),
+      ),
+    );
+  }
+}
