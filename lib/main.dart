cb2f1ec76aeeccc474af232dc3699c442fd83bae
RubenGomez1990 <rubengomez@paucasesnovescifp.cat>
2025-12-16 14:41:05 +0100

    Creación MyApp y home_page

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,19 @@
+import 'package:flutter/material.dart';
+
 void main() {
-  print('Hello, World!');
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text('My Flutter App')),
+        body: Center(child: Text('Hello, World!')),
+      ),
+    );
+  }
 }
